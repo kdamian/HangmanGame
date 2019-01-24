1abf1a94d6710c56aca18caf55f73b25cd0f5de5
Damian <blinkinruzdecaber@gmail.com>
2019-01-24 21:40:39 +0100

    creating the main panel

--- Hangman/Form1.cs
+++ Hangman/Form1.cs
@@ -16,5 +16,29 @@ namespace Hangman
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void draw_word()
+        {
+            string word;
+            string[] words = { "krokusy", "liliput", "krakers" };
+            int howManyWords = words.Length;
+
+            Random gen = new Random();
+            int IndexOfWord = gen.Next(0, howManyWords);
+
+            word = words[IndexOfWord];
+            label1.Text = Convert.ToString(word[0]);
+            label7.Text = Convert.ToString(word[6]);
+        }
     }
 }
